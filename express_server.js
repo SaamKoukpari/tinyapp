8aaef1716f6d4f78778c5eee5126cd7bc0d1a760
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-28 18:14:00 -0700

    add login/logout and cookies feature

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 // const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const generateRandomString = function(length = 6) {
   return Math.random().toString(36).substr(2, length) 
@@ -17,6 +19,18 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls")
+  console.log(req.body.username)
+});
+
+app.post("/logout", (req,res) => {
+  res.cookie('username', req.body.username);
+  res.clearCookie('username', req.body.username);
+  res.redirect("urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -43,17 +57,28 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const username = req.cookies.username
+  const templateVars = { 
+    username,
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies.username
+  const templateVars = { 
+    username };
+  res.render("urls_new", templateVars);
+  res.redirect("urls");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const username = req.cookies.username;
+  const templateVars = { 
+    username,
+    shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+  res.redirect("urls");
 });
 
 app.get("/urls.json", (req, res) => {
