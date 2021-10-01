f35ed058c94991d6fe94ecdb71ab91fddf8fed5c
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-01 13:31:13 -0700

    implement mocha chai test for helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
-const { getUserByEmail } = require('../helpers.js');
+const { findUserByEmail, generateRandomString, urlsForUser } = require('../helpers.js');
+
 
 const testUsers = {
   "userRandomID": {
@@ -14,11 +15,40 @@ const testUsers = {
   }
 };
 
+const testDatabase = {
+  'shortURLTest': {
+    longURL: 'longURL',
+    userID: 'userRandomID',
+  },
+  'shortURLTest2': {
+    longURL: 'longerURL',
+    userID: 'user2RandomID',
+  }
+};
+
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", users)
-    const expectedOutput = "userRandomID";
-    // Write your assert statement here
+    const user = findUserByEmail("user@example.com", testUsers)
+    const expectedOutput = "user@example.com"
+    assert.equal(user.email, expectedOutput);
+  });
+  it('should return undefined for a user with invalid email', function() {
+    const user = findUserByEmail("jack@gmail.com", testUsers)
+    const expectedOutput = undefined
+    assert.equal(user, expectedOutput);
   });
 });
 
+describe('generateRandomString', function() {
+  it('should return a string with 6 characters', function() {
+    assert.strictEqual(generateRandomString().length, 6);
+  });
+});
+
+describe('urlsForUser', function() {
+  it('should return an empty object if there are no matches', function() {
+    const urls = urlsForUser('noMatch', testDatabase);
+    const expectedOutput = '{}';
+    assert.strictEqual(JSON.stringify(urls), expectedOutput);
+  });
+});
